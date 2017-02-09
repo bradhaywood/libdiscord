59cddce269f99a8e6d100e25af5869af1a445458
Brad Haywood <brad.haywood@opusvl.com>
2017-02-09 08:52:56 +0000

    JSON stuff finally working

--- src/discord.h
+++ src/discord.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <jansson.h>
 #include "header.h"
 #include "http/http.h"
 
@@ -15,6 +16,19 @@ void discord_connect(struct Discord *discord) {
 		exit(1);
 	}
 	else {
+        json_t *root;
+        json_error_t error;
+        root = json_loads(r.content, 0, &error);
+        if (!root) {
+            printf("error: on line %d: %s\n", error.line, error.text);
+            exit(1);
+        }
+
+        json_t *data, *wsurl, *shards;
+        shards = json_object_get(root, "shards");
+        wsurl  = json_object_get(root, "url");
+        printf("Shards: \"%" JSON_INTEGER_FORMAT "\"\n", json_integer_value(shards));
+        printf("WS URL: %s\n", json_string_value(wsurl));
 		printf("%s\n", r.content);
 	}
 }
